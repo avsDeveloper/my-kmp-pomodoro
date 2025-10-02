6bc732d12b71a777c65c50a6fab69f8abe3bda2b
avsDeveloper <108736941+avsDeveloper@users.noreply.github.com>
2025-10-02 18:58:50 +0300

    Add Desktop app build configuration

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose)
+}
+
+group = "com.avsdeveloper.pomodoro"
+version = "1.0.0"
+
+dependencies {
+    implementation(projects.shared)
+    implementation(compose.desktop.currentOs)
+    implementation(libs.koin.core)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.avsdeveloper.pomodoro.desktop.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Pomodoro Timer"
+            packageVersion = "1.0.0"
+            
+            macOS {
+                iconFile.set(project.file("icon.icns"))
+            }
+            windows {
+                iconFile.set(project.file("icon.ico"))
+            }
+            linux {
+                iconFile.set(project.file("icon.png"))
+            }
+        }
+    }
+}
